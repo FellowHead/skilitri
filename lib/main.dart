91aa05d5496d17039fe4ae067624636db271689c
FellowHead <thefellowhead@gmail.com>
2019-07-07 16:05:00 +0200

    i/o system, selection mode, node bodies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
 import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:skilitri/tree.dart';
 import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
+import 'package:path_provider/path_provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -24,6 +29,10 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.teal,
+        primaryColor: Color(0xff90ffa0),
+        primaryColorDark: Color(0xffd0e0e0),
+        backgroundColor: Color(0xffa0a0a0),
+        buttonColor: Color(0xff6070c0)
       ),
       home: Skilitri()
     );
@@ -42,9 +51,13 @@ class ShapesPainter extends CustomPainter {
     final paint = Paint();
     paint.color = Colors.black;
     paint.strokeWidth = 5;
+
     for (Node n in root.getDescendants()) {
       if (n.parent is Node) {
-        canvas.drawLine(n.position.scale(1, -1), (n.parent as Node).position.scale(1, -1), paint);
+        Offset start = n.position.scale(1, -1);
+        Offset end = (n.parent as Node).position.scale(1, -1);
+        canvas.drawLine(start, end, paint);
+        canvas.drawCircle(Offset.lerp(start, end, 0.7), 25, paint);
       }
     }
   }
@@ -63,28 +76,32 @@ class Skilitri extends StatefulWidget {
 class SkilitriState extends State<Skilitri> {
   bool check = false;
   Root tree;
+  bool inSelectionMode = false;
+  Set<Node> selection = {};
+  Node active;
 
   void resetTree() {
-    tree = Root(this,
+    tree = Root(
         {
           Node(
               title: "Node",
               position: Offset(0, 0),
+              body: DemoBody(),
               children: {
-                ScoreNode(
+                Node(
                     title: "Score Node #1",
                     position: Offset(0, -100),
-                    score: 100,
+                    body: ScoreBody(score: 100),
                     children: {
-                      ScoreNode(
-                          title: "Score Node #2",
-                          position: Offset(-300, -200),
-                          score: 5
+                      Node(
+                        title: "Score Node #2",
+                        position: Offset(-300, -200),
+                        body: ScoreBody(score: 5),
                       ),
-                      ScoreNode(
-                          title: "Score Node #3",
-                          position: Offset(300, -200),
-                          score: 63
+                      Node(
+                        title: "Score Node #3",
+                        position: Offset(300, -200),
+                        body: ScoreBody(score: 63),
                       )
                     }
                 )
@@ -117,12 +134,116 @@ class SkilitriState extends State<Skilitri> {
                   .y / sc);
               notifier.value++;
             },
-            child: Center(
-              child: n.render(notifier),
+            child: GestureDetector(
+                onTapUp: (details) =>
+                {
+                  print("onTapUp on " + n.toString()),
+                  if (inSelectionMode) {
+                    if (selection.contains(n)) {
+                      if (selection.length == 1) {
+                        exitSelectionMode()
+                      } else
+                        {
+                          selection.remove(n),
+                          if (active == n) {
+                            active = null
+                          }
+                        }
+                    } else
+                      {
+                        select(n, false)
+                      },
+                    notifier.value++
+                  }
+                },
+                onTapDown: (details) => {
+                  print("28 tap wounds")
+                },
+                onLongPressStart: (details) =>
+                {
+                  Feedback.forLongPress(context),
+                  select(n, true)
+                },
+                child: Center(
+                  child: n.render(Theme.of(context), notifier, getSelectionType(n)),
+                )
             )
         )
     );
   }
+
+  SelectionType getSelectionType(Node n) {
+    if (active == n) {
+      return SelectionType.Focused;
+    } else if (selection.contains(n)) {
+      return SelectionType.Selected;
+    } else {
+      return SelectionType.None;
+    }
+  }
+
+  Offset screenToView(BuildContext ctx, Offset sc) {
+    Matrix4 m = matrix.clone();
+    var zoom = MatrixGestureDetector.decomposeToValues(m).scale;
+    m.translate(-sc.dx / zoom, (150 - sc.dy) / zoom, 0);
+    return Offset(-m.getTranslation().x / zoom - 180, m.getTranslation().y / zoom + 220);
+  }
+
+  void select(Node n, bool deselectOthers) {
+    setState(() =>
+    {
+      if (deselectOthers) {
+        selection = {}
+      },
+      selection.add(n),
+      inSelectionMode = true,
+      active = n
+    });
+  }
+
+  void createEmptyNode(Offset position) {
+    Node n = Node(
+        title: "NEW NODE",
+        position: position,
+        body: DemoBody()
+    );
+    tree.addChild(n);
+    select(n, true);
+    //notifier.value++;
+  }
+
+  // I/O STUFF
+
+  _read() async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final file = File('${directory.path}/tree.fwd');
+      String text = await file.readAsString();
+      print(text);
+      setState(() => {
+        tree = Root.fromJson(jsonDecode(text))
+      });
+    } on FileSystemException catch(e) {
+      print("Can't read file");
+    }
+  }
+
+  _save() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/tree.fwd');
+    final text = jsonEncode(tree.toJson());
+    await file.writeAsString(text);
+    print('saved');
+
+    Fluttertoast.showToast(
+      msg: "Skilltree gespeichert!",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIos: 1,
+    );
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -145,10 +266,26 @@ class SkilitriState extends State<Skilitri> {
                       onPressed: () =>
                       {
                         resetTree(),
-                        notifier.value++
+                        exitSelectionMode(),
                       },
                       child: Text('Reset tree'),
-                    )
+                    ),
+                    IconButton(
+                      onPressed: () => {
+                        _save()
+                      },
+                      icon: Icon(
+                          Icons.save
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () => {
+                        _read()
+                      },
+                      icon: Icon(
+                          Icons.restore_page
+                      ),
+                    ),
                   ],
                 ),
                 height: 50,
@@ -163,40 +300,194 @@ class SkilitriState extends State<Skilitri> {
                                   matrix, tm, sm, null);
                           notifier.value++;
                         },
-                        child: Container(
-                            width: double.infinity,
-                            height: double.infinity,
-                            alignment: Alignment.topLeft,
-                            color: Color(0xff444444),
-                            child: AnimatedBuilder(
-                                animation: notifier,
-                                builder: (ctx, child) {
-                                  return Container(
-                                      width: double.infinity,
-                                      height: double.infinity,
-                                      child: Stack(
-                                          fit: StackFit.expand,
-                                          children: [
-                                            buildCanvas(),
-                                            Stack(
-                                              children: tree.getDescendants()
-                                                  .map((n) =>
-                                                  buildNode(n)
-                                              ).toList(),
-                                            )
-                                          ]
-                                      )
-                                  );
-                                }
-                            )
+                        child: GestureDetector(
+                          onLongPressStart: (details) =>
+                          {
+                            Feedback.forLongPress(context),
+                            if (inSelectionMode) {
+                              exitSelectionMode()
+                            } else {
+                              createEmptyNode(
+                                  screenToView(context, details.globalPosition))
+                            }
+                          },
+                          behavior: HitTestBehavior.opaque,
+                          child: Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              alignment: Alignment.topLeft,
+                              color: Theme.of(context).backgroundColor,
+                              child: AnimatedBuilder(
+                                  animation: notifier,
+                                  builder: (ctx, child) {
+                                    return Container(
+                                        width: double.infinity,
+                                        height: double.infinity,
+                                        child: Stack(
+                                            fit: StackFit.expand,
+                                            children: [
+                                              buildCanvas(),
+                                              Stack(
+                                                children: tree.getDescendants()
+                                                    .map((n) =>
+                                                    buildNode(n)
+                                                ).toList(),
+                                              )
+                                            ]
+                                        )
+                                    );
+                                  }
+                              )
+                          ),
                         )
                     );
                   },
                 ),
               ),
+              // SELECTION MODE BAR
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.decelerate,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10
+                    )
+                  ]
+                ),
+                child: Row(
+                  children: <Widget>[
+                    MaterialButton(
+                      onPressed: () =>
+                      {
+                        setState(() => {
+                          exitSelectionMode()
+                        })
+                      },
+                      child: Text('Exit'),
+                    ),
+                    IconButton(
+                      onPressed: () => {
+                        setState(() => {
+                          for (Node n in selection) {
+                            n.remove(true)
+                          },
+                          exitSelectionMode()
+                        })
+                      },
+                      icon: Icon(
+                        Icons.delete
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: selection.length != 1 ? () => {
+                        setState(() => {
+                          if (active != null) {
+                            for (Node n in selection) {
+                              if (n != active) {
+                                if (!active.getAscendants().contains(n)) {
+                                  active.addChild(n),
+                                } else {
+                                  print('puh knappe nummer')
+                                }
+                              }
+                            },
+                          },
+                        })
+                      } : null,
+                      icon: Icon(
+                          Icons.link
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () => {
+                        setState(() => {
+                          for (Node n in selection) {
+                            n.setParent(tree)
+                          },
+                        })
+                      },
+                      icon: Icon(
+                          Icons.link_off
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: selection.length == 1 ? () => {
+                        setState(() => {
+                          _showDialog(selection.first)
+                        })
+                      } : null,
+                      icon: Icon(
+                          Icons.edit
+                      ),
+                    ),
+                  ],
+                ),
+                height: inSelectionMode ? 50 : 0,
+              ),
             ]
         )
     );
+  }
+
+  void _showToast(BuildContext context) {
+    final scaffold = Scaffold.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text('Added to favorite'),
+        action: SnackBarAction(
+            label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+      ),
+    );
+  }
+
+  void _showActualToast() {
+  }
+
+  void _showDialog(Node n) {
+    final TextEditingController controller = TextEditingController(text: n.title);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Node bearbeiten"),
+          content: Container(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Node benamseln..."
+                  ),
+                  onChanged: (s) => {
+                    n.title = s
+                  },
+                  controller: controller
+                )
+              ],
+            ),
+            height: 100.0,
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void exitSelectionMode() {
+    setState(() {
+      inSelectionMode = false;
+      active = null;
+      selection = {};
+    });
   }
 
   Matrix4 matrix = Matrix4.identity();
--- lib/tree.dart
+++ lib/tree.dart
@@ -1,17 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'main.dart';
 
 class Root extends Parent {
-  State state;
+  Root([Set<Node> children]) : super(children);
 
-  Root(State state, [Set<Node> children]) : super(children) {
-    this.state = state;
+  @override
+  String toString() {
+    return "TREE ROOT";
   }
+
+  Root.fromJson(Map<String, dynamic> json)
+      : super(Parent._childrenFromJson(json));
+  //toJson() => super.toJson();
 }
 
-class Parent {
-  Set<Node> children;
+enum SelectionType {
+  None,
+  Selected,
+  Focused,
+  Dragged
+}
+
+abstract class Parent {
+  Set<Node> _children;
 
   Parent([Set<Node> children]) {
     if (children == null) {
@@ -21,32 +34,95 @@ class Parent {
         n.parent = this;
       }
     }
-    this.children = children;
+    this._children = children;
   }
 
   Set<Node> getDescendants() {
     Set<Node> out = Set();
-    if (children.length > 0) {
-      for (Node n in children) {
+    if (_children.length > 0) {
+      for (Node n in _children) {
         out.addAll(n.getDescendants());
+        out.add(n);
       }
-    }
-    if (this is Node) {
-      out.add(this);
     }
     return out;
   }
+
+  void addChild(Node n) {
+    //print("Adding " + n.toString() + " to " + toString());
+    if (n.parent != null) {
+      n.parent._children.remove(n);
+    }
+    n.parent = this;
+    _children.add(n);
+  }
+
+  static Set<Node> _childrenFromJson(Map<String, dynamic> json) {
+    return (json['children'] as List<dynamic>).map((f) => Node.fromJson(f)).toSet();
+  }
+
+  Map<String, dynamic> toJson() =>
+      {
+        'children': _children.map((f) => f.toJson()).toList(growable: false),
+      };
 }
 
 class Node extends Parent {
   String title;
   Offset position;
   Parent parent;
+  NodeBody _body;
 
-  Node({@required String title, @required Offset position, Set<Node> children})
+  Node({@required String title, @required Offset position, @required NodeBody body, Set<Node> children})
       : super(children) {
     this.title = title;
     this.position = position;
+    setBody(body);
+  }
+
+
+  Node.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        position = Offset(json['position']['x'], json['position']['y']), super(Parent._childrenFromJson(json)) {
+    setBody(NodeBody.decipher(json['body']));
+  }
+
+  Map<String, dynamic> toJson() => {
+        'title': title,
+        'position': {
+          'x': position.dx,
+          'y': position.dy
+        },
+        'body': {
+          'type': _body.getTypeId(),
+          'content': _body.toJson()
+        }
+      }..addAll(super.toJson());
+
+  void setBody(NodeBody body) {
+    _body = body;
+    _body._init(this);
+  }
+
+  void setParent(Parent p) {
+    if (parent != null) {
+      parent._children.remove(this);
+    }
+    if (p != null) {
+      p.addChild(this);
+    }
+    parent = p;
+  }
+
+  Set<Parent> getAscendants() {
+    Set<Parent> out = Set();
+    Parent p = parent;
+    while (p is Node) {
+      out.add(p);
+      p = (p as Node).parent;
+    }
+    out.add(p);
+    return out;
   }
 
   Root getTreeRoot() {
@@ -57,12 +133,20 @@ class Node extends Parent {
     return p;
   }
 
-  Widget render(ValueNotifier notifier) {
+  Widget render(ThemeData theme, ValueNotifier notifier, SelectionType sel) {
     return Container(
-      width: 125,
+      width: 225,
       height: 100,
       decoration: BoxDecoration(
-          color: Colors.red
+        color: theme.buttonColor,
+        border: Border.all(width: 5, color: sel == SelectionType.None ? Color(0x0) : (sel == SelectionType.Focused ? theme.primaryColor : theme.primaryColorDark)),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 15,
+            spreadRadius: 5,
+            color: Colors.black26
+          )
+        ]
       ),
       child: Center(
         child: Column(
@@ -71,10 +155,10 @@ class Node extends Parent {
               style: TextStyle(
                   color: Colors.white,
                   //fontSize: 15.0 / scale
-                  fontSize: 17.0
+                  fontSize: 18.0
               ),
             ),
-            renderBody(notifier)
+            _body.renderBody(notifier)
           ],
         ),
       ),
@@ -83,13 +167,77 @@ class Node extends Parent {
 
   bool check = false;
 
+  @override
+  String toString() {
+    return "Node(" + title + ")";
+  }
+
+  void remove(bool keepChildren) {
+    if (keepChildren) {
+      for (Node n in _children.toSet()) {
+        parent.addChild(n);
+      }
+    } else {
+      for (Node n in _children.toSet()) {
+        n.remove(false);
+      }
+    }
+    _children = {};
+    parent._children.remove(this);
+    parent = null;
+  }
+}
+
+abstract class NodeBody {
+  Node _node;
+
+  NodeBody() {
+
+  }
+
+  static NodeBody decipher(Map<String, dynamic> json) {
+    dynamic content = json['content'];
+    switch(json['type']) {
+      case 'demo':
+        return DemoBody.fromJson(content);
+      case 'score':
+        return ScoreBody.fromJson(content);
+    }
+    print('UNKNOWN NODE TYPE "' + json['type'] + '"');
+    return DemoBody();
+  }
+  
+  void _init(Node node) {
+    _node = node;
+  }
+
+  Widget renderBody(ValueNotifier notifier);
+
+  NodeBody.fromJson(Map<String, dynamic> json);
+  Map<String, dynamic> toJson();
+  String getTypeId();
+}
+
+class DemoBody extends NodeBody {
+  bool check = false;
+
+  DemoBody() : super();
+
+  DemoBody.fromJson(Map<String, dynamic> json)
+      : check = json['check'];
+
+  Map<String, dynamic> toJson() =>
+      {
+        'check': check,
+      };
+
+  @override
   Widget renderBody(ValueNotifier notifier) {
     return Column(
       children: <Widget>[
         Checkbox(
           onChanged: (v) =>
           {
-            print("click"),
             check = v,
             notifier.value++
           },
@@ -98,20 +246,33 @@ class Node extends Parent {
       ],
     );
   }
+
+  @override
+  String getTypeId() {
+    return "demo";
+  }
 }
 
-class ScoreNode extends Node {
+class ScoreBody extends NodeBody {
   int score;
 
-  ScoreNode({@required String title, int score = 0, @required Offset position, Set<Node> children}) : super(title: title, position: position, children: children) {
+  ScoreBody({int score = 0}) : super() {
     this.score = score;
   }
 
+  ScoreBody.fromJson(Map<String, dynamic> json)
+      : score = json['score'];
+
+  Map<String, dynamic> toJson() =>
+      {
+        'score': score,
+      };
+
   int getTotalScore() {
     int out = score;
-    for (Node n in getDescendants()) {
-      if (n is ScoreNode) {
-        out += n.score;
+    for (Node n in _node.getDescendants()) {
+      if (n._body is ScoreBody) {
+        out += (n._body as ScoreBody).score;
       }
     }
     return out;
@@ -130,8 +291,9 @@ class ScoreNode extends Node {
       ],
     );
   }
-}
 
-class EnumNode extends Node {
-
+  @override
+  String getTypeId() {
+    return "score";
+  }
 }
